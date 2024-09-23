82d9566ddbde7d0ffceae26f2b441c56698fef6e
React-Props <daniel.linker@intellicode-solutions.de>
2024-09-23 11:01:45 +0200

    style ausgabe

---
+++ Angular/Ausgabe_von_Daten/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular';
+
+  user = {
+    name: 'Sascha',
+    age: 25,
+  }
+
+  tooltip = 'Ich bin eine Link'
+
+  inlineStyles = {
+    width: '50%',
+    background: 'yellow'
+  }
+
+  cssClass = 'blue'
+
+  withtext = 50;
+}
+
+
