8ab97d67372044ee3ebcfb29fe626490a01279ea
React-Props <daniel.linker@intellicode-solutions.de>
2024-10-16 21:27:16 +0200

    auth

--- Projekte_Angular/tik-tolk/src/app/app.component.ts
+++ Projekte_Angular/tik-tolk/src/app/app.component.ts
@@ -1,9 +1,6 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {ProfileCardComponent} from "./common-ui/profile-card/profile-card.component";
-import {ProfileService} from "./data/services/profile.service";
-import {JsonPipe} from "@angular/common";
-import {Profile} from "./data/interfaces/profile.interfaces";
 
 @Component({
   selector: 'app-root',
@@ -15,14 +12,4 @@ import {Profile} from "./data/interfaces/profile.interfaces";
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  profileService = inject(ProfileService)
-  profiles: Profile[] = []
-
-
-  constructor() {
-    this.profileService.getTestAccout()
-      .subscribe(value => {
-        this.profiles = value
-      })
-  }
 }
---
+++ Projekte_Angular/tik-tolk/src/app/pages/search-page/search-page.component.ts
@@ -0,0 +1,28 @@
+import {Component, inject} from '@angular/core';
+import {ProfileCardComponent} from "../../common-ui/profile-card/profile-card.component";
+import {ProfileService} from "../../data/services/profile.service";
+import {Profile} from "../../data/interfaces/profile.interfaces";
+
+@Component({
+  selector: 'app-search-page',
+  standalone: true,
+  imports: [
+    ProfileCardComponent
+  ],
+  templateUrl: './search-page.component.html',
+  styleUrl: './search-page.component.scss'
+})
+export class SearchPageComponent {
+
+  profileService = inject(ProfileService)
+  profiles: Profile[] = []
+
+
+  constructor() {
+    this.profileService.getTestAccout()
+      .subscribe(value => {
+        this.profiles = value
+      })
+  }
+
+}
